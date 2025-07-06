976031fda7eecff1e9238aac75e1092ec53d3002
GTK-kala <gtkkala1110@gmail.com>
2025-07-06 15:26:16 +0300

    Seventh commit

--- React-Food-App/src/Components/ExploreMenu/Menu.jsx
+++ React-Food-App/src/Components/ExploreMenu/Menu.jsx
@@ -1,9 +1,24 @@
+import './Menu.css'
+import { menu_list } from '../../assets/frontend_assets/assets'
 
-
-const Menu = () => {
+const Menu = ({category, setCategory}) => {
   return (
-    <div>
-        
+    <div className='menu' id='menu'>
+        <h1>Explore our menu</h1>
+        <p className="menu-text">Choose from a diverse menu featuring delectable array of dishes.Our mission is to satisfy your cravings and elevate your dining experience.one delicious meal at a time</p>
+        <div className="menu-list">
+            {
+            menu_list.map((item,i) => {
+                return (
+                    <div className="menu-items" onClick={() => setCategory(prev=> prev === item.menu_name ? 'All' : item.menu_name)} key={i}>
+                        <img className={category === item.menu_name ? 'active' : ''} src={item.menu_image} alt={item.name} />
+                        <p>{item.menu_name}</p>
+                    </div>
+                 )
+              })
+             }
+        </div>
+      <hr />
     </div>
   )
 }
--- React-Food-App/src/Pages/Home/Home.jsx
+++ React-Food-App/src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Header from '../../Components/Header/Header'
+import Menu from '../../Components/ExploreMenu/Menu'
+import { useState } from 'react'
 import './Home.css'
 
 const Home = () => {
+  const [category, setCategory] = useState('All')
   return (
     <div>
       <Header />
+      <Menu category={category} setCategory={setCategory} />
     </div>
   )
 }
