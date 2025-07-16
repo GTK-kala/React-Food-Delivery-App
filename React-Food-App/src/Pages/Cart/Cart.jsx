ed1c350111011ece63306a3f7bf362a2ed339c79
GTK-kala <gtkkala1110@gmail.com>
2025-07-16 19:58:54 +0300

    Eighteenth commit

--- React-Food-App/src/Pages/Cart/Cart.jsx
+++ React-Food-App/src/Pages/Cart/Cart.jsx
@@ -3,7 +3,9 @@ import "./Cart.css";
 import { StoreContext } from "../../Context/StoreContext";
 
 const Cart = () => {
+
   const { cartItems, food_list, RemoveFromCart } = useContext(StoreContext);
+
   return (
     <div className="cart">
       <div className="cart-items">
@@ -24,16 +26,47 @@ const Cart = () => {
                 <div className="cart-items-title cart-items-item">
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>{item.price}</p>
+                  <p>$ {item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
-                  <p className="cross">x</p>
+                  <p>$ {item.price * cartItems[item._id]}</p>
+                  <p className="cross" onClick={() =>RemoveFromCart(item._id)}>x</p>
                 </div>
                 <hr />
               </div>
             );
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Dlivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code , Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code"/>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
